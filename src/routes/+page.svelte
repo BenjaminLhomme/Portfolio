707c5d2559f0614021e1a2a72b79bc695a916f69
B3nj4m1Ns <47667079+B3nj4m1Ns@users.noreply.github.com>
2023-09-04 14:28:03 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div id="particles-js"></div>
+<div class="partie-gauche">
+    <div class="centre">
+        <h1>Lhomme Benjamin</h1>
+        <h2>Développeur</h2>
+        <ul class="liste-centree">
+            <li><a href="/competence">Compétences</a></li>
+            <li><a href="/LanguageFramework">Langages et Framework</a></li>
+            <li><a href="/portfolio">Projets</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </div>
+</div>
+<hr class="separation" />
+<div class="partie-droite">
+    <div class="columns is-centered">
+        <div class="column is-narrow">
+            <div class="notification is-primary bulle-dialogue">
+                <p class="has-text-centered">Bienvenue sur ce site web, je serais ici pour vous expliquer <br> son fonctionnement ci besoin, vous aurez juste à me cliquer dessus. <br> Bonne Visite.</p>
+            </div>
+            <div id="lottie-animation-1" class="lottie-container"></div>
+        </div>
+    </div>
+</div>
+
+<style>
+    @import "../style/home.css";
+</style>
